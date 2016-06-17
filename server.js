671a1c8783ee2f5edfd4bd28535b2be5382283d4
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-06-16 17:55:53 -0700

    add ?completed filter

--- server.js
+++ server.js
@@ -9,9 +9,23 @@ var todoNextId = 1;
 
 app.use(bodyParser.json()); // post
 
-//post
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
+});
+
+//GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed:true})
+	}
+	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed:false})
+	}
+
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {
