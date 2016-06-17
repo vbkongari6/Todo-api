f5e2cc1521276269a2114056ebca670533190dd7
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-06-17 15:23:06 -0700

    add sequelize

--- server.js
+++ server.js
@@ -9,32 +9,39 @@ var todoNextId = 1;
 
 app.use(bodyParser.json()); // post
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
 //GET /todos?completed=true@q=work
-app.get('/todos', function (req, res) {
+app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed:true});
-	}
-	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed:false});
+		filteredTodos = _.where(filteredTodos, {
+			completed: true
+		});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {
+			completed: false
+		});
 	}
 
 	if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
-		filteredTodos = _.filter(filteredTodos, function (todo) { return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; });
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodos);
 });
 
-app.get('/todos/:id', function (req, res) {
+app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
 	/*var matchedTodo;
 	
@@ -44,10 +51,9 @@ app.get('/todos/:id', function (req, res) {
 		}
 	});*/
 
-	if (matchedTodo) {		
+	if (matchedTodo) {
 		res.json(matchedTodo);
-	}
-	else {
+	} else {
 		res.status(404).send();
 	}
 });
@@ -55,7 +61,7 @@ app.get('/todos/:id', function (req, res) {
 // POST /todos
 // post methods require body-parser
 // npm install body-parser@1.13.3 --save
-app.post('/todos', function (req, res) {
+app.post('/todos', function(req, res) {
 	//var body = req.body;
 	var body = _.pick(req.body, 'description', 'completed');
 	//console.log('description: ' + body.description);
@@ -74,40 +80,43 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
-app.delete('/todos/:id', function (req, res) {
+app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
 	if (!matchedTodo) {
-		res.status(404).json({"error": "no todo found with that id"});
-	}
-	else {
-		todos = _.without(todos, matchedTodo);	
+		res.status(404).json({
+			"error": "no todo found with that id"
+		});
+	} else {
+		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
 });
 
-app.put('/todos/:id', function (req, res) {
+app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};	
+	var validAttributes = {};
 
 	if (!matchedTodo) {
 		return res.status(404).send();
-	}	
+	}
 
 	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
 		validAttributes.completed = body.completed;
-	}
-	else if (body.hasOwnProperty('completed')) {
+	} else if (body.hasOwnProperty('completed')) {
 		return res.status(400).send();
 	}
 
 	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
 		validAttributes.description = body.description;
-	}
-	else if (body.hasOwnProperty('description')) {
+	} else if (body.hasOwnProperty('description')) {
 		return res.status(400).send();
 	}
 
@@ -115,6 +124,6 @@ app.put('/todos/:id', function (req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
 	console.log('Express listening to port ' + PORT + '!');
 });
---
+++ server.js using get method/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'meet gf for dinner',
+	completed: false
+}, {
+	id: 2,
+	description: 'go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'watch a movie',
+	completed: true
+}];
+
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+	
+	todos.forEach( function (todo) {
+		if (todo.id === todoId) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {		
+		res.json(matchedTodo);
+	}
+	else {
+		res.status(404).send();
+	}
+
+	/*res.send('Asking for todo with id of ' + todoId);*/
+});
+
+app.listen(PORT, function () {
+	console.log('Express listening to port ' + PORT + '!');
+});
