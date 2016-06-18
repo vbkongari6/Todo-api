e3e15e1a1111a5a57416e2a87cd40d2c48fe646c
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-06-17 23:41:09 -0700

    convert GET /todos with sequelize

--- server.js
+++ server.js
@@ -16,7 +16,30 @@ app.get('/', function(req, res) {
 
 //GET /todos?completed=true@q=work
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
+	var query = req.query;
+	var where = {};
+
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} 
+	else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+			where.completed = false;
+	}
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		}
+	}
+
+	db.todo.findAll({where: where}).then( function (todos) {
+		res.json(todos);
+	}, function (e) {
+		res.status(500).send();
+	});
+	
+	// or
+	/*var queryParams = req.query;
 	var filteredTodos = todos;
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
@@ -35,7 +58,7 @@ app.get('/todos', function(req, res) {
 		});
 	}
 
-	res.json(filteredTodos);
+	res.json(filteredTodos);*/
 });
 
 app.get('/todos/:id', function(req, res) {
