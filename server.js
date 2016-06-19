5e3cdd1b71c30c65d8c26991b905d6a9dac81d36
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-06-19 16:09:26 -0700

    convert CRUD request to use current user todos only

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.get('/', function(req, res) {
 //app.get('/todos', function(req, res) {
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	//var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -67,7 +70,13 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then( function (todo) {
+	//db.todo.findById(todoId).then( function (todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then( function (todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		}	
@@ -137,7 +146,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then( function (rowsDeleted) {
 		if (rowsDeleted === 0) { 
@@ -180,7 +190,13 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then( function (todo) {
+	//db.todo.findById(todoId).then( function (todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}		
+	}).then( function (todo) {
 		if (todo) {
 			todo.update(attributes).then( function (todo) {
 				res.json(todo.toJSON())
